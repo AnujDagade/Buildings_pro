0fa8c9c40e9ab1e053a01e94380497f81eeddbc9
AnujDagade <dagadeanuj@gmail.com>
2022-11-05 14:52:18 +0000

    DONE

--- app/src/main/java/mooc/vandy/java4android/buildings/logic/Building.java
+++ app/src/main/java/mooc/vandy/java4android/buildings/logic/Building.java
@@ -6,6 +6,62 @@ package mooc.vandy.java4android.buildings.logic;
 public class Building {
 
     // TODO - Put your code here.
+    private int mLength;
+    private int mWidth;
+    private int mLotLength;
+    private int mLotWidth;
 
-    
+    public Building(int length, int width, int lotLength, int lotWidth) {
+        mLength = length;
+        mWidth = width;
+        mLotLength = lotLength;
+        mLotWidth = lotWidth;
+    }
+
+    public int getLength() {
+        return mLength;
+    }
+
+    public int getWidth() {
+        return mWidth;
+    }
+
+    public int getLotLength() {
+        return mLotLength;
+    }
+
+    public int getLotWidth() {
+        return mLotWidth;
+    }
+
+    public void setLength(int length) {
+        mLength = length;
+    }
+
+    public void setWidth(int width) {
+        mWidth = width;
+    }
+
+    public void setLotLength(int length) {
+        mLotLength = length;
+    }
+
+    public void setLotWidth(int width) {
+        mLotWidth = width;
+    }
+
+    public int calcBuildArea() {
+        return mLength * mWidth;
+    }
+
+    public int calcLotArea() {
+        return mLotLength * mLotWidth;
+    }
+
+    @Override
+    public String toString() {
+
+        return "a "+ mLength+ "x" +mWidth +" building";
+    }
+
 }
--- app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
+++ app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
@@ -3,9 +3,50 @@ package mooc.vandy.java4android.buildings.logic;
 /**
  * This is the House class file that extends Building.
  */
-public class House
-       extends Building {
+public class House extends Building {
 
     // TODO - Put your code here.
+
+    private String mOwner;
+    private boolean mPool;
+
+    public House(int length, int width, int lotLength, int lotWidth) {
+        super(length, width, lotLength, lotWidth);
+        mPool = false;
+    }
+
+
+    public House(int length, int width, int lotLength, int lotWidth, String owner) {
+        super(length, width, lotLength, lotWidth);
+        mOwner = owner;
+
+    }
+
+    public House(int length, int width, int lotLength, int lotWidth, String owner, boolean pool) {
+        super(length, width, lotLength, lotWidth);
+        mOwner = owner;
+        mPool = pool;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public boolean hasPool() {
+        return mPool;
+    }
+
+    public void setmOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public void setPool(boolean pool) {
+        mPool = pool;
+    }
+
+    @Override
+    public String toString() {
+        return
+    }
     
 }
